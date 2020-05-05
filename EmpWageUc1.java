ede85b747d7640d385b4ac1db6838a29d78d036a
shrawanamit <amitkumar06111@gmail.com>
2020-05-05 15:10:37 +0530

    commit [use case 5,6] calculating wages of month and match the condition of working hours

--- EmpWageUc1.java
+++ EmpWageUc1.java
@@ -4,12 +4,17 @@ public class EmpWageUc1 {
 			final int is_part_time=1;
 			final int is_full_time=2;	
          int emp_rate_per_hours=20;
+			int max_hours_per_month=100;
+			int no_of_working_days=20;
+			int total_Emp_hours=0;
+ 			int total_working_days=0;
          int empHrs=0;
          int empWage=0;
+			while(total_Emp_hours < max_hours_per_month && total_working_days < no_of_working_days) {
 
-         int empCheck =(int)Math.floor(Math.random()*10)%3;
+         	int empCheck =(int)Math.floor(Math.random()*10)%3;
 
-		   switch (empCheck) {
+		   	switch (empCheck) {
                 case is_full_time:
                     empHrs = 8;
                     break;
@@ -19,7 +24,11 @@ public class EmpWageUc1 {
                 default:
                     empHrs = 0;
             }
-			empWage = emp_rate_per_hours * empHrs;
+			
+				total_working_days ++;
+         	total_Emp_hours += empHrs;
+				empWage += emp_rate_per_hours * empHrs;
+			}
 			System.out.println("total Emp Wages is:- " + empWage );
     }
 }
