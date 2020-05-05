9d493fafcf2ac509a10ad89cf1debff51996f09f
shrawanamit <amitkumar06111@gmail.com>
2020-05-05 14:57:42 +0530

    commit [use case 2,3,4] calculating wages of part and full time using switch

--- EmpWageUc1.java
+++ EmpWageUc1.java
@@ -1,12 +1,25 @@
 public class EmpWageUc1 {
     public static void main(String[] args) {
-        int is_full_time=1;
-        int  empCheck =(int) Math.floor(Math.random()*10)%2;
-        if (empCheck == is_full_time) {
-            System.out.println("Emp is present");
-        } else {
-            System.out.println("Emp is Absent");
-        }
 
-   }
+			final int is_part_time=1;
+			final int is_full_time=2;	
+         int emp_rate_per_hours=20;
+         int empHrs=0;
+         int empWage=0;
+
+         int empCheck =(int)Math.floor(Math.random()*10)%3;
+
+		   switch (empCheck) {
+                case is_full_time:
+                    empHrs = 8;
+                    break;
+                case is_part_time:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+			empWage = emp_rate_per_hours * empHrs;
+			System.out.println("total Emp Wages is:- " + empWage );
+    }
 }
